9d0b9be8b36f99d35e3e799a8842cf7a904d0fa5
Rob Alcaraz <robertoalcaraz@irba.es>
2025-06-23 18:59:54 +0200

    save images

--- code/plots.R
+++ code/plots.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
 # titanic_analysis.R
-# Reads Titanic data and produces several ggplot2 visualizations
+# Reads Titanic data and produces several ggplot2 visualizations,
+# saving all plots into an 'images/' folder.
 
 # Load required libraries
 if (!require("tidyverse")) {
@@ -10,12 +11,18 @@ if (!require("tidyverse")) {
 }
 
 # Read the data
+if (!file.exists("data/titanic.csv")) {
+  stop("data/titanic.csv not found. Please ensure the file exists.")
+}
 titanic <- read.csv("data/titanic.csv", stringsAsFactors = FALSE)
 
 # Convert relevant columns to factors
 titanic$Survived <- factor(titanic$Survived, levels = c(0,1), labels = c("No", "Yes"))
 titanic$Pclass   <- factor(titanic$Pclass, levels = c(1,2,3), labels = c("1st", "2nd", "3rd"))
 titanic$Sex      <- factor(titanic$Sex)
+
+# Create images directory if it doesn't exist
+dir.create("images", showWarnings = FALSE)
 
 # 1. Bar plot: Survival count by passenger class
 p1 <- ggplot(titanic, aes(x = Pclass, fill = Survived)) +
@@ -25,6 +32,8 @@ p1 <- ggplot(titanic, aes(x = Pclass, fill = Survived)) +
        y = "Count",
        fill = "Survived") +
   theme_minimal()
+# Save plot
+ggsave(filename = "images/survival_by_class.png", plot = p1, width = 6, height = 4)
 
 # 2. Age distribution histogram colored by survival
 p2 <- ggplot(titanic, aes(x = Age, fill = Survived)) +
@@ -33,6 +42,8 @@ p2 <- ggplot(titanic, aes(x = Age, fill = Survived)) +
        x = "Age",
        y = "Count") +
   theme_minimal()
+# Save plot
+ggsave(filename = "images/age_distribution_by_survival.png", plot = p2, width = 6, height = 4)
 
 # 3. Boxplot of Fare by Survival
 p3 <- ggplot(titanic, aes(x = Survived, y = Fare, fill = Survived)) +
@@ -41,6 +52,8 @@ p3 <- ggplot(titanic, aes(x = Survived, y = Fare, fill = Survived)) +
        x = "Survived",
        y = "Fare (USD)") +
   theme_minimal()
+# Save plot
+ggsave(filename = "images/fare_distribution_by_survival.png", plot = p3, width = 6, height = 4)
 
 # 4. Proportion bar: Sex vs. Survival
 p4 <- ggplot(titanic, aes(x = Sex, fill = Survived)) +
@@ -49,9 +62,8 @@ p4 <- ggplot(titanic, aes(x = Sex, fill = Survived)) +
        x = "Sex",
        y = "Proportion") +
   theme_minimal()
+# Save plot
+ggsave(filename = "images/proportion_survival_by_sex.png", plot = p4, width = 6, height = 4)
 
-# Print plots to the default graphics device
-print(p1)
-print(p2)
-print(p3)
-print(p4)
+message("All plots saved to 'images/' folder.")
+
